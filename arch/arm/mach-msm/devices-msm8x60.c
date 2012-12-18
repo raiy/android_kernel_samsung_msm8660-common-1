bc192726aa34094d5d81ff8d8389c4c131ba119e
aaa <aaa@aaa-desktop.(none)>
2012-12-18 19:10:24 +0900

    Revert "gpu overclock"
    This reverts commit ceff5fe7221fd2a7db4212b22e3011700193482d.

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -621,7 +621,7 @@ static struct msm_bus_vectors grp3d_nominal_high_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(2484),
+		.ib = KGSL_CONVERT_TO_MBPS(2008),
 	},
 };
 
@@ -630,7 +630,7 @@ static struct msm_bus_vectors grp3d_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(2976),
+		.ib = KGSL_CONVERT_TO_MBPS(2484),
 	},
 };
 
@@ -677,7 +677,7 @@ static struct msm_bus_vectors grp2d0_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(2048),
+		.ib = KGSL_CONVERT_TO_MBPS(990),
 	},
 };
 
@@ -712,7 +712,7 @@ static struct msm_bus_vectors grp2d1_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE1,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(2048),
+		.ib = KGSL_CONVERT_TO_MBPS(990),
 	},
 };
 
@@ -767,24 +767,14 @@ static struct resource kgsl_3d0_resources[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 320000000,
-			.bus_freq = 4,
-			.io_fraction = 33,
-		},
-		{
-			.gpu_freq = 300000000,
-			.bus_freq = 4,
-			.io_fraction = 50,
-		},
-		{
 			.gpu_freq = 266667000,
 			.bus_freq = 4,
-			.io_fraction = 33,
+			.io_fraction = 0,
 		},
 		{
 			.gpu_freq = 228571000,
 			.bus_freq = 3,
-			.io_fraction = 50,
+			.io_fraction = 33,
 		},
 		{
 			.gpu_freq = 200000000,
@@ -802,7 +792,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 0,
-	.num_levels = 7,
+	.num_levels = 5,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
 	.nap_allowed = true,
@@ -840,14 +830,6 @@ static struct resource kgsl_2d0_resources[] = {
 static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 266667000,
-			.bus_freq = 1,
-		},
-		{
-			.gpu_freq = 228571000,
-			.bus_freq = 1,
-		},		
-		{
 			.gpu_freq = 200000000,
 			.bus_freq = 1,
 		},
@@ -857,7 +839,7 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 		},
 	},
 	.init_level = 0,
-	.num_levels = 4,
+	.num_levels = 2,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.nap_allowed = true,
@@ -895,14 +877,6 @@ static struct resource kgsl_2d1_resources[] = {
 static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 266667000,
-			.bus_freq = 1,
-		},
-		{
-			.gpu_freq = 228571000,
-			.bus_freq = 1,
-		},		
-		{
 			.gpu_freq = 200000000,
 			.bus_freq = 1,
 		},
@@ -912,7 +886,7 @@ static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 		},
 	},
 	.init_level = 0,
-	.num_levels = 4,
+	.num_levels = 2,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.nap_allowed = true,
